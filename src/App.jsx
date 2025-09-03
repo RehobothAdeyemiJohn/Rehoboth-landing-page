a96a2eafe0188b95638481aa13e0d6ed678d0fc7
unknown <johnoluwapelumi1@gmail.com>
2025-09-03 10:35:14 +0100

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Hero from './Components/Hero'
 import Analytics from './Components/Analytics'
 import Newsletter from './Components/Newsletter'
 import Cards from './Components/Card'
-import Footer from './Components/Footerr'
+import Footerr from './Components/Footerr'
 
 
 
